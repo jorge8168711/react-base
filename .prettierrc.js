6e58e89a1101b72f6226361ac38aefd4b7f3779c
Jorge Barron <jorgeb8168711@gmail.com>
2020-07-23 20:15:44 -0500

    Prettier and eslint

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports =  {
+  "trailingComma": "none",
+  "tabWidth": 2,
+  "semi": true,
+  "singleQuote": true,
+  "jsxSingleQuote": true,
+  "jsxBracketSameLine": true,
+  "printWidth": 100
+};
